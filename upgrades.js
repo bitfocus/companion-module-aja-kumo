67505d79f797edd91bfd5b75fecfb11e59909301
Justin Osborne <justin@eblah.com>
2022-11-30 12:31:11 -0600

    verify that config isn't null when upgrading

--- upgrades.js
+++ upgrades.js
@@ -8,34 +8,37 @@ export default [
 
     // We need to try to find the max number of src/dest this kumo model supports
     function(context, props) {
-        // The minimum kumo has these specs; anything over 16 src is a x:x (ie, 32x32)
-        let max_src = 16
-        let max_dest = 4
         const config = props.config
 
-        props.actions.forEach(x => {
-            if (x.options && 'source' in x.options) {
-                if (max_src < parseInt(x.options.source)) max_src = parseInt(x.options.source)
+        if (config !== null) {
+            // The minimum kumo has these specs; anything over 16 src is a x:x (ie, 32x32)
+            let max_src = 16
+            let max_dest = 4
+
+            props.actions.forEach(x => {
+                if (x.options && 'source' in x.options) {
+                    if (max_src < parseInt(x.options.source)) max_src = parseInt(x.options.source)
+                }
+
+                if (x.options && 'destination' in x.options) {
+                    if (max_dest < parseInt(x.options.destination)) max_dest = parseInt(x.options.destination)
+                }
+            })
+
+            max_src = Math.max(max_src, max_dest)
+            if(max_dest == 4 && max_src == 16) {
+                // Kumo 1604 model
+            } else if (max_src == 16) {
+                max_src = max_dest = 16
+            } else if (max_src <= 32) {
+                max_src = max_dest = 32
+            } else {
+                max_src = max_dest = 64
             }
 
-            if (x.options && 'destination' in x.options) {
-                if (max_dest < parseInt(x.options.destination)) max_dest = parseInt(x.options.destination)
-            }
-        })
-
-        max_src = Math.max(max_src, max_dest)
-        if(max_dest == 4 && max_src == 16) {
-            // Kumo 1604 model
-        } else if (max_src == 16) {
-            max_src = max_dest = 16
-        } else if (max_src <= 32) {
-            max_src = max_dest = 32
-        } else {
-            max_src = max_dest = 64
+            config.src_count = String(max_src)
+            config.dest_count = String(max_dest)
         }
-
-        config.src_count = String(max_src)
-        config.dest_count = String(max_dest)
 
         return {
             updatedConfig: config,
